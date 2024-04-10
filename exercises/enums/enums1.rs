535e73a9f46629cdc88fc5fc10e0e5ee6e7ad8d3
jafar75 <gholamzadeh.cuh@gmail.com>
2024-04-10 17:40:06 +0330

    before hashmaps!

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -2,15 +2,18 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit = 12,
+    Echo = 34,
+    Move = 36,
+    ChangeColor = 11
 }
 
 fn main() {
-    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::Quit as u32);
     println!("{:?}", Message::Echo);
     println!("{:?}", Message::Move);
     println!("{:?}", Message::ChangeColor);
