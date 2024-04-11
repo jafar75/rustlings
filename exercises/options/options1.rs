3aa224d901d44f60e5f057a21728ff94ebeb728f
jafar75 <gholamzadeh.cuh@gmail.com>
2024-04-11 18:52:31 +0330

    done options exercise

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -3,17 +3,29 @@
 // Execute `rustlings hint options1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // This function returns how much icecream there is left in the fridge.
 // If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
 // all, so there'll be no more left :(
 fn maybe_icecream(time_of_day: u16) -> Option<u16> {
+    use std::cmp::Ordering;
     // We use the 24-hour system here, so 10PM is a value of 22 and 12AM is a
     // value of 0 The Option output should gracefully handle cases where
     // time_of_day > 23.
     // TODO: Complete the function body - remember to return an Option!
-    ???
+
+    let out;
+    if time_of_day > 23 {
+        return None;
+    }
+
+    match time_of_day.cmp(&22) {
+        Ordering::Less => out = Some(5),
+        Ordering::Greater => out = Some(0),
+        Ordering::Equal => out = Some(0),
+    };
+
+    out
 }
 
 #[cfg(test)]
@@ -33,7 +45,7 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get at the value contained in the
         // Option?
-        let icecreams = maybe_icecream(12);
+        let icecreams = maybe_icecream(12).unwrap();
         assert_eq!(icecreams, 5);
     }
 }
